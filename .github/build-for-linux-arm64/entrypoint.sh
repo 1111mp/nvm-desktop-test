e8518b5955bb42d5de208a503872162923d673a6
The1111mp <The1111mp@outlook.com>
2024-09-05 18:03:27 +0800

    fix: release
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- .github/build-for-linux-arm64/entrypoint.sh
+++ .github/build-for-linux-arm64/entrypoint.sh
@@ -11,14 +11,11 @@ npm install pnpm -g
 
 rustup target add "$INPUT_TARGET"
 
-if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
-    apt-get update
-    apt-get install -y libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf
-elif [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
+if [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
     dpkg --add-architecture arm64
     apt update
-    apt install -y pkg-config libglib2.0-dev:arm64 libncurses6:arm64 libtinfo6:arm64 linux-libc-dev:arm64 libncursesw6:arm64 libssl3:arm64 libcups2:arm64
-    apt install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross libwebkit2gtk-4.1-dev:arm64 libgtk-3-dev:arm64 patchelf:arm64 librsvg2-dev:arm64 libayatana-appindicator3-dev:arm64
+    apt install -y pkg-config libglib2.0-dev libncurses6 libtinfo6 linux-libc-dev libncursesw6 libssl3 libcups2
+    apt install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross libwebkit2gtk-4.1-dev libappindicator3-dev libgtk-3-dev patchelf librsvg2-dev libayatana-appindicator3-dev
     export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
     export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
     export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
---
+++ .github/build-for-linux-x64/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 设置 Rust 默认工具链为稳定版本
+rustup default stable
+rustup --version
+
+wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
+tar -Jxvf ./node-v20.17.0-linux-x64.tar.xz
+export PATH=$(pwd)/node-v20.17.0-linux-x64/bin:$PATH
+npm install pnpm -g
+
+rustup target add "$INPUT_TARGET"
+
+if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
+    apt-get update
+    apt-get install -y libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf
+else
+    echo "Unknown target: $INPUT_TARGET" && exit 1
+fi
+
+bash .github/build-for-linux/build.sh
