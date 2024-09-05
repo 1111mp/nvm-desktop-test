09df7cb5e306a6f30e670c642a60ad708a307378
The1111mp <The1111mp@outlook.com>
2024-09-05 19:43:03 +0800

    fix: release
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- .github/build-for-linux-arm64/entrypoint.sh
+++ .github/build-for-linux-arm64/entrypoint.sh
@@ -11,19 +11,9 @@ npm install pnpm -g
 
 rustup target add "$INPUT_TARGET"
 
-if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
+if [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
     apt-get update
     apt-get install -y libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf
-elif [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
-    dpkg --add-architecture arm64
-    apt-get update
-    apt-get install -y pkg-config libglib2.0-dev libncurses6 libtinfo6 linux-libc-dev libncursesw6 libssl3 libcups2
-    apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross libwebkit2gtk-4.1-dev libappindicator3-dev libgtk-3-dev patchelf librsvg2-dev libayatana-appindicator3-dev
-    export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
-    export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
-    export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
-    export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
-    export PKG_CONFIG_ALLOW_CROSS=1
 else
     echo "Unknown target: $INPUT_TARGET" && exit 1
 fi
