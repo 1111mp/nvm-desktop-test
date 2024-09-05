2780d922d9ff44772738ab6194b3a0cbecf53590
The1111mp <The1111mp@outlook.com>
2024-09-05 16:21:28 +0800

    fix: release
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- .github/build-for-linux/entrypoint.sh
+++ .github/build-for-linux/entrypoint.sh
@@ -17,7 +17,7 @@ if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
 elif [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
     dpkg --add-architecture arm64
     apt-get update
-    apt-get install -y libncurses6:arm64 libtinfo6:arm64 linux-libc-dev:arm64 libncursesw6:arm64 libssl3:arm64 libcups2:arm64
+    apt-get install -y pkg-config libglib2.0-dev:arm64 libncurses6:arm64 libtinfo6:arm64 linux-libc-dev:arm64 libncursesw6:arm64 libssl3:arm64 libcups2:arm64
     apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross libgtk-3-dev:arm64 patchelf:arm64 librsvg2-dev:arm64 libayatana-appindicator3-dev:arm64
     export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
     export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
