65583b9d7049cf07950c69aab3c9893efb59092f
The1111mp <The1111mp@outlook.com>
2024-09-05 16:08:07 +0800

    fix: release
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- .github/build-for-linux/entrypoint.sh
+++ .github/build-for-linux/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 设置 Rust 默认工具链为稳定版本
+rustup default stable
+rustup --version
+
 wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
 tar -Jxvf ./node-v20.17.0-linux-x64.tar.xz
 export PATH=$(pwd)/node-v20.17.0-linux-x64/bin:$PATH
@@ -9,12 +13,12 @@ rustup target add "$INPUT_TARGET"
 
 if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
     apt-get update
-    apt-get install -y libwebkit2gtk-4.0-dev libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf
+    apt-get install -y libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf
 elif [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
     dpkg --add-architecture arm64
     apt-get update
     apt-get install -y libncurses6:arm64 libtinfo6:arm64 linux-libc-dev:arm64 libncursesw6:arm64 libssl3:arm64 libcups2:arm64
-    apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross libwebkit2gtk-4.0-dev:arm64 libgtk-3-dev:arm64 patchelf:arm64 librsvg2-dev:arm64 libayatana-appindicator3-dev:arm64
+    apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross libgtk-3-dev:arm64 patchelf:arm64 librsvg2-dev:arm64 libayatana-appindicator3-dev:arm64
     export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
     export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
     export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
