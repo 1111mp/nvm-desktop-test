ecc3913c7d5a206f9554c9eddeb3b46acc4181d7
The1111mp <31227919+1111mp@users.noreply.github.com>
2024-09-05 12:18:02 +0800

    Update entrypoint.sh

--- .github/build-for-linux/entrypoint.sh
+++ .github/build-for-linux/entrypoint.sh
@@ -9,25 +9,21 @@ rustup target add "$INPUT_TARGET"
 
 if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
     sudo apt-get update
-    sudo apt-get install -y wget file libglib2.0-dev libgtk-3-dev libwebkit2gtk-4.1-dev libxdo-dev build-essential libssl-dev libappindicator3-dev libayatana-appindicator3-dev librsvg2-dev patchelf
+    sudo apt-get install -y curl wget file libglib2.0-dev libgtk-3-dev libwebkit2gtk-4.1-dev libxdo-dev build-essential libssl-dev libappindicator3-dev libayatana-appindicator3-dev librsvg2-dev patchelf
+		export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/share/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig
+		export PKG_CONFIG_ALLOW_CROSS=1
 elif [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
     sudo dpkg --add-architecture arm64
     sudo apt-get update
     sudo apt-get install -y libncurses6:arm64 libtinfo6:arm64 linux-libc-dev:arm64 libncursesw6:arm64 libssl3:arm64 libcups2:arm64
-    sudo apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross wget:arm64 file:arm64 libglib2.0-dev:arm64 libwebkit2gtk-4.1-dev:arm64 libxdo-dev:arm64 build-essential:arm64 libssl-dev:arm64 libappindicator3-dev:arm64 libgtk-3-dev:arm64 patchelf:arm64 librsvg2-dev:arm64 libayatana-appindicator3-dev:arm64
+    sudo apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross curl:arm64 wget:arm64 file:arm64 libglib2.0-dev:arm64 libwebkit2gtk-4.1-dev:arm64 libxdo-dev:arm64 build-essential:arm64 libssl-dev:arm64 libappindicator3-dev:arm64 libgtk-3-dev:arm64 patchelf:arm64 librsvg2-dev:arm64 libayatana-appindicator3-dev:arm64
     export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
     export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
     export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
-    export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
+    export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/share/pkgconfig:/usr/lib/aarch64-linux-gnu/pkgconfig
     export PKG_CONFIG_ALLOW_CROSS=1
 else
     echo "Unknown target: $INPUT_TARGET" && exit 1
-fi
-
-# Verify that glib-2.0 is found
-if ! pkg-config --exists glib-2.0; then
-    echo "glib-2.0 not found. Please ensure it is installed."
-    exit 1
 fi
 
 bash .github/build-for-linux/build.sh
