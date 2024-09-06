f363f2e7ba416a50ac63592b320149329088757a
The1111mp <The1111mp@outlook.com>
2024-09-06 13:36:39 +0800

    fix: release
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- .github/build-for-linux-arm64/entrypoint.sh
+++ .github/build-for-linux-arm64/entrypoint.sh
@@ -12,10 +12,39 @@ npm install pnpm -g
 rustup target add "$INPUT_TARGET"
 
 if [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
+		apt-get update
+		apt-get install -y gcc-aarch64-linux-gnu
+
 		dpkg --add-architecture arm64
+		# 替换 sources.list 文件内容
+		# 使用 'cat' 替换 sources.list 文件内容
+		sudo bash -c 'cat <<EOL > /etc/apt/sources.list
+		deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy main restricted
+		deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy-updates main restricted
+		deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy universe
+		deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy-updates universe
+		deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy multiverse
+		deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy-updates multiverse
+		deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse
+		deb [arch=amd64] http://security.ubuntu.com/ubuntu/ jammy-security main restricted
+		deb [arch=amd64] http://security.ubuntu.com/ubuntu/ jammy-security universe
+		deb [arch=amd64] http://security.ubuntu.com/ubuntu/ jammy-security multiverse
+
+		# 添加 Ubuntu ports 源
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy main restricted
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy-updates main restricted
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy universe
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy-updates universe
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy multiverse
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy-updates multiverse
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy-backports main restricted universe multiverse
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy-security main restricted
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy-security universe
+		deb [arch=armhf,arm64] http://ports.ubuntu.com/ubuntu-ports jammy-security multiverse
+		EOL'
+
     apt-get update
 		apt-get upgrade -y
-		apt-get install -y gcc-aarch64-linux-gnu
 		apt-get install -y libssl-dev:arm64
     apt-get install -y libwebkit2gtk-4.1-dev:arm64 libappindicator3-dev:arm64 librsvg2-dev:arm64 patchelf:arm64
 		apt-get --fix-broken install
