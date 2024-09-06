5c9f75459716f61899778a129dc2b8b78d385587
The1111mp <The1111mp@outlook.com>
2024-09-06 11:31:13 +0800

    fix: release
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- .github/build-for-linux-arm64/entrypoint.sh
+++ .github/build-for-linux-arm64/entrypoint.sh
@@ -4,16 +4,23 @@
 rustup default stable
 rustup --version
 
-wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-arm64.tar.xz
-tar -Jxvf ./node-v20.17.0-linux-arm64.tar.xz
-export PATH=$(pwd)/node-v20.17.0-linux-arm64/bin:$PATH
+wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
+tar -Jxvf ./node-v20.17.0-linux-x64.tar.xz
+export PATH=$(pwd)/node-v20.17.0-linux-x64/bin:$PATH
 npm install pnpm -g
 
 rustup target add "$INPUT_TARGET"
 
 if [ "$INPUT_TARGET" = "aarch64-unknown-linux-gnu" ]; then
+		dpkg --add-architecture arm64
     apt-get update
-    apt-get install -y libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf
+		apt-get install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu libssl3:arm64
+    apt-get install -y libwebkit2gtk-4.1-dev:arm64 libappindicator3-dev:arm64 librsvg2-dev:arm64 patchelf:arm64
+		export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
+    export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
+    export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
+		export PKG_CONFIG_SYSROOT_DIR=/usr/aarch64-linux-gnu/:/usr/lib/aarch64-linux-gnu/pkgconfig
+		export PKG_CONFIG_ALLOW_CROSS=1
 else
     echo "Unknown target: $INPUT_TARGET" && exit 1
 fi
